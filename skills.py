1e14d49b45d7a8faef00d4322e678cc71802bcdd
Hackbright Student <info@hackbrightacademy.com>
2015-04-18 18:27:06 -0700

    Implemented word_length function; tests passed

--- skills.py
+++ skills.py
@@ -31,7 +31,7 @@ def count_unique(string1):
     """
 
     return Counter(string1.split(" ")) # Split string into list, create a Counter from list.
-                                       
+
 
 
 
@@ -178,8 +178,21 @@ def word_length(words):
         [(1, ['a']), (2, ['ok', 'an']), (3, ['day']), (5, ['apple'])]
 
     """
-    # 
-    return []
+    word_length_dict = {}
+    word_length_list = []
+
+    for word in words:
+        word_length_dict.setdefault(len(word), []) # for each word in list of words, adds its lenght to word_length_dict dictionary with empty list
+                                                   # as value; if already in dictionary, no changes made (returns value)
+        word_length_dict[len(word)].append(word)   # for each word in list of words, appends the word to the value list belonging to the key
+                                                    # that is the length of the word
+
+    for length in word_length_dict.keys(): # iterates through a list of word lengths, the word_length_dict keys (word_length_dict.keys())
+        word_tuple = (length, word_length_dict[length])  # for each length, Python finds it in word_length_dict and makes a tuple of
+                                                         # the length and a list of the words of that length
+        word_length_list.append(word_tuple)              # appends the tuple to word_length_list 
+
+    return sorted(word_length_list)                      # returns a sorted copy of word_length_list
 #
 #
 # def pirate_talk(phrase):
--- testskills.py
+++ testskills.py
@@ -1,31 +1,30 @@
-def sum_zero(list1):
+def word_length(words):
+    """Given list of words, return list of ascending [(len, [words])].
 
-    # all_sums = {}
+    Given a list of words, return a list of tuples, ordered by word-length.
+    Each tuple should have two items--the length of the words for that
+    word-length, and the list of words of that word length.
 
-    # for index, num in enumerate(list1):
-    #     for next_num in list1[index + 1:]:
-    #         sum = num + next_num
-    #         all_sums.setdefault(sum, [])
-    #         pair_list = sorted([num, next_num])
-    #         all_sums[sum].append(tuple(pair_list))
-    # print all_sums
-           
+    For example:
 
+        >>> word_length(["ok", "an", "apple", "a", "day"])
+        [(1, ['a']), (2, ['ok', 'an']), (3, ['day']), (5, ['apple'])]
 
-    all_sums = set()
+    """
+    word_length_dict = {}
+    word_length_list = []
 
-    for index, num in enumerate(list1):
-        for next_num in list1[index + 1:]:
-            sum = num + next_num
-            if sum == 0:
-                all_sums.add(tuple(sorted([num, next_num])))
+    for word in words:
+        word_length_dict.setdefault(len(word), []) # for each word in list of words, adds its lenght to word_length_dict dictionary with empty list
+                                                   # as value; if already in dictionary, no changes made (returns value)
+        word_length_dict[len(word)].append(word)   # for each word in list of words, appends the word to the value list belonging to the key
+                                                    # that is the length of the word
 
-    # return list(all_sums)
+    for length in word_length_dict.keys(): # iterates through a list of word lengths, the word_length_dict keys (word_length_dict.keys())
+        word_tuple = (length, word_length_dict[length])  # for each length, Python finds it in word_length_dict and makes a tuple of
+                                                         # the length and a list of the words of that length
+        word_length_list.append(word_tuple)              # appends the tuple to word_length_list 
 
+    return sorted(word_length_list)                      # returns a sorted copy of word_length_list
 
-            # all_sums.setdefault(sum, [])
-            # pair_list = sorted([num, next_num])
-            # all_sums[sum].append(tuple(pair_list))
-    print list(all_sums)           
-
-sum_zero([1, 2, 3, -2, -1, 1, 0, 1, 0])
+word_length(["ok", "an", "apple", "a", "day"])
